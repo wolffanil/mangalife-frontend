6329b745633a96dfdb9adc78ce3ef2ab958a44d2
wolffanil2 <wolffanil@mail.ru>
2025-06-08 15:02:24 +0400

    fix static

--- src/app/(site)/manga/[id]/page.tsx
+++ src/app/(site)/manga/[id]/page.tsx
@@ -7,10 +7,6 @@ import Manga from '@/features/manga/Manga'
 import { API_URL, getMangaUrl } from '@/shared/config/api.config'
 import { PUBLIC_URL } from '@/shared/config/url.config'
 import { APP_URL } from '@/shared/libs/constants/url.constants'
-import {
-	fetchNewMangas,
-	fetchPopularMangas
-} from '@/shared/services/fetchMangas'
 import type { IMangaById } from '@/shared/types/manga.interface'
 
 const getManga = cache(async (id: string): Promise<IMangaById> => {
@@ -23,19 +19,19 @@ const getManga = cache(async (id: string): Promise<IMangaById> => {
 	return data.manga
 })
 
-export async function generateStaticParams() {
-	const [newMangas, popularMangas] = await Promise.all([
-		fetchNewMangas(),
-		fetchPopularMangas()
-	])
+// export async function generateStaticParams() {
+// 	const [newMangas, popularMangas] = await Promise.all([
+// 		fetchNewMangas(),
+// 		fetchPopularMangas()
+// 	])
 
-	const mangas = [
-		...newMangas?.map(manga => ({ id: manga._id })),
-		...popularMangas?.map(manga => ({ id: manga._id }))
-	]
+// 	const mangas = [
+// 		...newMangas?.map(manga => ({ id: manga._id })),
+// 		...popularMangas?.map(manga => ({ id: manga._id }))
+// 	]
 
-	return mangas
-}
+// 	return mangas
+// }
 
 export async function generateMetadata({
 	params
