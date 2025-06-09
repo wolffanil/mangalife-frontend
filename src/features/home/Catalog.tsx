d74411b90cf506f699fdf429eff769b412bc7724
wolffanil2 <wolffanil@mail.ru>
2025-06-09 15:22:21 +0400

    fix h catalog

--- src/features/home/Catalog.tsx
+++ src/features/home/Catalog.tsx
@@ -7,7 +7,10 @@ async function Catalog() {
 	//
 
 	return (
-		<WhiteWrapper title='Каталог' className='mt-[76px] xl:mt-[157px]'>
+		<WhiteWrapper
+			title='Каталог'
+			className='mt-[76px] max-sm:min-h-[630px] xl:mt-[157px]'
+		>
 			<div className='mt-[67px] flex w-full flex-col items-start gap-y-[30px] xl:mt-[135px] xl:gap-y-[50px]'>
 				<PopularMangas />
 				<NewMangas />
